d295f0d9e64a730ae7fc95a3354391511110b212
Rodrigo Ferreira <jferreira@imaginarycloud.com>
2021-01-12 15:36:47 +0000

    validate phone number

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,8 @@ class ApplicationController < ActionController::API
 
   # Tests
 
+  #curl -d '["+1983236248"]' "http://localhost:3000/aggregate"
+
   def aggregate
     result = {}
 
@@ -23,39 +25,40 @@ class ApplicationController < ActionController::API
 
       phones_list.each do |phone|
 
-        # TODO validate Number
-        # For invalid numbers, the API replies with a bad request status code. A number is considered valid if it contains only digits, an optional leading + and
-        # whitespace anywhere except immediately after the +. A valid number has exactly 3 digits or more than 6 and less than 13. 00 is acceptable as replacement for the leading +.
-
-        begin
-          api_phone_response = Rails.cache.fetch("sector/#{phone}", expires_in: 24.hours) do
-            call_business_sector_api phone
-          end
-        rescue Exception => e
-          render json: {status: "error", code: 503, message: e.message}
-          return
-        end
-
-        if api_phone_response.code == "200"
-          phone_specification = JSON.parse(api_phone_response.body)
-
-          # Get Prefix
-          prefix = retrive_prefix phone
-
-          unless prefix.blank?
-            # Create Hash for prefix if no exists
-            result[prefix] = {} unless result.key?(prefix)
-
-            # Fill Inner Hash for prefix
-            if result[prefix][phone_specification["sector"]].blank?
-              result[prefix][phone_specification["sector"]] = 1
-            else
-              result[prefix][phone_specification["sector"]] += 1
+        # Validate Phone Number
+        if valid_phone(phone)
+          begin
+            api_phone_response = Rails.cache.fetch("sector/#{phone.delete(" ")}", expires_in: 24.hours) do
+              call_business_sector_api phone
             end
+          rescue Exception => e
+            render json: {status: "error", code: 503, message: e.message}
+            return
           end
 
+          if api_phone_response.code == "200"
+            phone_specification = JSON.parse(api_phone_response.body)
+
+            # Get Prefix
+            prefix = retrieve_prefix phone
+
+            unless prefix.blank?
+              # Create Hash for prefix if no exists
+              result[prefix] = {} unless result.key?(prefix)
+
+              # Fill Inner Hash for prefix
+              if result[prefix][phone_specification["sector"]].blank?
+                result[prefix][phone_specification["sector"]] = 1
+              else
+                result[prefix][phone_specification["sector"]] += 1
+              end
+            end
+
+          else
+            logger.error "#{api_phone_response.code} - #{api_phone_response.message} - phone number #{phone}"
+          end
         else
-          logger.error "#{api_phone_response.code} - #{api_phone_response.message} - phone number #{phone}"
+          logger.info "Invalid phone number #{phone}"
         end
 
       end
@@ -69,19 +72,43 @@ class ApplicationController < ActionController::API
 
   private
 
-  def fix_plus_symbol phone
+  def remove_plus_symbol phone
     phone.starts_with?("+") ? phone.gsub("+", "") : phone
   end
 
-  def fix_double_zero phone
+  def remove_double_zero phone
     phone.starts_with?("00") ? phone.sub("00", "") : phone
   end
 
-  def retrive_prefix phone
-    phone_changed = fix_plus_symbol phone
-    phone_changed = fix_double_zero phone_changed
-    PREFIXES.each do |p|
-      return p.strip if phone_changed.starts_with?(p.strip) #remove newline char with strip
+  def valid_phone phone
+    phone = phone.strip #remove leading and trailing whitespace
+    return false if !(phone.starts_with?("+") || phone.starts_with?("00"))  #return false - 00 is acceptable as replacement for the leading +
+    return false if phone.starts_with?("+ ") #return false if whitespace immediately after the +
+
+    phone = remove_plus_symbol phone
+    phone = remove_double_zero phone
+    phone = phone.delete(" ") #delete whitespaces
+
+    if (phone =~ /\A\d+\Z/) == 0 # only numbers
+      #phone = phone.sub(prefix, "") #remove prefix
+      if phone.length == 3
+        return true
+      elsif phone.length >= 6 && phone.length <= 13
+        return true
+      end
+    else
+      return false
+    end
+
+    return false
+  end
+
+  def retrieve_prefix phone
+    phone = phone.strip() #remove leading and trailing whitespace
+    phone = remove_plus_symbol phone
+    phone = remove_double_zero phone
+    PREFIXES.each do |prefix|
+      return prefix.strip if phone.starts_with?(prefix)
     end
     logger.info "Invalid number: #{phone} (Prefix unknown)"
     ""
--- config/initializers/load_prefixes.rb
+++ config/initializers/load_prefixes.rb
@@ -1,7 +1,7 @@
 PREFIXES = []
 File.open("prefixes.txt", "r") do |f|
   f.each_line do |line|
-    PREFIXES << line if line.length > 0
+    PREFIXES << line.strip if line.length > 0
   end
 end
 PREFIXES = PREFIXES.uniq
